1683e92b240ba0e72fd2cad222c1ceb28195cdfd
ZachH155 <zachary.hulan@keyin.com>
2024-09-19 21:42:46 -0230

    added help flag as well as the help message for each flag

--- app.js
+++ app.js
@@ -24,8 +24,91 @@ if (!process.argv[2]) {
 //for loop that cycles through process.argv to check for flags
 for (let i = 0; i < process.argv.length; i++) {
 
+    //help flag
+    if (process.argv[i].includes("--help")) {
+        let helponly = true
+        
+        if (process.argv.includes("--length")) {
+            helponly = false
+
+            console.log(`
+                The length flag is used to change the length of the
+                generated password, by deafult the length is 8.
+
+                This is how to use the length flag:
+                
+                "--length=[number] --generate"
+                `)
+        }
+        if (process.argv.includes("--capitalize")) {
+            helponly = false
+
+            console.log(`
+                The capitalize flag is used to add capital letters
+                to the generated password.
+
+                This is how to use the capitalize flag:
+                
+                "--capitalize --generate"
+                `)
+        }
+        if (process.argv.includes("--numbers")) {
+            helponly = false
+
+            console.log(`
+                The numbers flag is used to add numbers
+                to the generated password.
+                
+                This is how to use the numbers flag:
+                
+                "--numbers --generate"
+                `)
+        }
+        if (process.argv.includes("--symbols")) {
+            helponly = false
+
+            console.log(`
+                The symbols flag is used to add symbols
+                to the generated password.
+                
+                This is how to use the symbols flag:
+                
+                "--symbols --generate"
+                `)
+        }
+        if (process.argv.includes("--generate")) {
+            helponly = false
+
+            console.log(`
+                The generate flag is used to generate the password.
+                
+                This is how to use the generate flag:
+                
+                "--generate"
+                `)
+        }
+        if (helponly == true) {
+            console.log(`
+                This program is used to generate passwords, so you don't have to!
+
+                Need help with using the other flags? This is how
+                to use the help flag:
+                
+                "--help [other flag]"
+
+                Example:
+
+                "--help --generate" 
+
+                Try it out!
+                `)
+        }
+
+    }
+    
+
     //length flag
-    if (process.argv[i].includes("--length")) {
+    if (process.argv[i].includes("--length") && !process.argv.includes("--help")) {
         var length = parseInt(process.argv[i].split("=")[1]);
         
         if (!isNaN(length)) {
@@ -41,7 +124,7 @@ for (let i = 0; i < process.argv.length; i++) {
 
 
     //password generation
-    if (process.argv[i].includes("--generate")) {
+    if (process.argv[i].includes("--generate") && !process.argv.includes("--help")) {
 
         let characters = "abcdefghijklmnopqrstuvwxyz";
         
